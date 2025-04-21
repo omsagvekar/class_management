149093d941ebd7140f5e02fb23eda5b232ab9812
omsagvekar <omsagvekar04@gmail.com>
2025-04-21 09:25:59 +0530

    --

--- app/dashboard.php
+++ app/dashboard.php
@@ -153,7 +153,7 @@
             <div>
                 <button class="logout" onclick="location.href='logout.php';"><i class="fa-solid fa-power-off"></i> Logout</button>
             </div>
-            <h1> Dashboard of Class Management </h1>
+            <h1> Dashboard of Class Management test 1</h1>
             <div class="metrics">
                 <div class="metric">
                     <h2>Total Fees Collected</h2>
